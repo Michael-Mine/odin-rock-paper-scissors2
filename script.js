b1adb66b9c87fc2e0c07559ddc9c203539389592
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2024-05-02 17:25:56 +0100

    Add part of 3rd function successfully

--- script.js
+++ script.js
@@ -8,15 +8,12 @@ function getHumanChoice() {
     return humanChoice.toLowerCase(); 
 }
 function getComputerChoice() {
-    
     computerChoice = Math.random()
     
     if (computerChoice <= 0.333){
         computerChoice = "rock"
-        
     } else if (computerChoice >= 0.666) {
         computerChoice = "paper"
-
     } else {
         computerChoice = "scissors"
     }
@@ -25,6 +22,14 @@ function getComputerChoice() {
 function playRound(humanChoice, computerChoice) {
     console.log("Human choice is " + humanChoice);
     console.log("Computer choice is " + computerChoice);
+
+    if (humanChoice === computerChoice) {
+        alert("This round is a tie!")
+    } else if (humanChoice === "rock" && computerChoice == "paper") {
+        alert("You win this round!")
+        humanScore++;
+        alert("Your score is " + humanScore + " and computer score is " + computerScore)
+    }
 }
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
