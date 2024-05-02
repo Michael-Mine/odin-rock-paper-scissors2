6ece5d403a1601e21cbf6349e22ff0b0d7ad24a5
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2024-05-02 16:43:32 +0100

    Tidy code and add start of 3rd function

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
+let humanScore = 0;
+let computerScore = 0; 
+let humanChoice;
 let computerChoice;
 
+function getHumanChoice() {
+    humanChoice = prompt("Make your Choice! Type rock, paper or scissors");
+    return humanChoice.toLowerCase(); 
+}
 function getComputerChoice() {
     
     computerChoice = Math.random()
     
     if (computerChoice <= 0.333){
-        computerChoice = "Rock"
+        computerChoice = "rock"
         
     } else if (computerChoice >= 0.666) {
-        computerChoice = "Paper"
+        computerChoice = "paper"
 
     } else {
-        computerChoice = "Scissors"
+        computerChoice = "scissors"
     }
-
-    return computerChoice
+    return computerChoice;
 }
-
-getComputerChoice();
-
-console.log("Computer choice is " + computerChoice);
-
-let humanChoice;
-
-function getHumanChoice() {
-    humanChoice = prompt("Make your Choice! Type rock, paper or scissors");
+function playRound(humanChoice, computerChoice) {
+    console.log("Human choice is " + humanChoice);
+    console.log("Computer choice is " + computerChoice);
 }
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
-getHumanChoice();
-
-console.log("Human choice is " + humanChoice) 
-
-
+playRound(humanSelection, computerSelection);
