09c336f6ed5170e8da08d6e4fb36358e5134a248
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2024-05-02 17:50:20 +0100

    Complete 3rd function to play a round

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let humanChoice;
 let computerChoice;
 
 function getHumanChoice() {
-    humanChoice = prompt("Make your Choice! Type rock, paper or scissors");
+    humanChoice = prompt("Make your choice, type: rock, paper or scissors");
     return humanChoice.toLowerCase(); 
 }
 function getComputerChoice() {
@@ -25,10 +25,20 @@ function playRound(humanChoice, computerChoice) {
 
     if (humanChoice === computerChoice) {
         alert("This round is a tie!")
-    } else if (humanChoice === "rock" && computerChoice == "paper") {
+    } else if ((humanChoice === "rock" && computerChoice == "scissors") ||
+            (humanChoice === "paper" && computerChoice == "rock") ||
+            (humanChoice === "scissors" && computerChoice == "paper")) {
         alert("You win this round!")
         humanScore++;
         alert("Your score is " + humanScore + " and computer score is " + computerScore)
+    } else if ((computerChoice === "rock" && humanChoice == "scissors") ||
+            (computerChoice === "paper" && humanChoice == "rock") ||
+            (computerChoice === "scissors" && humanChoice == "paper")) {
+        alert("You lose this round!")
+        computerScore++;
+        alert("Your score is " + humanScore + " and computer score is " + computerScore)
+    } else {
+        alert("Choice not recognised! Please type a valid choice of: rock, paper or scissors")
     }
 }
 const humanSelection = getHumanChoice();
